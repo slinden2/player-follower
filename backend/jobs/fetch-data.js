6a24905ef1e4cedd5f0b7b7ce96b17e94daf8abf
Samu <samu.linden@gmail.com>
2019-06-10 21:53:45 +0200

    Working on data fetching

--- backend/jobs/fetch-data.js
+++ backend/jobs/fetch-data.js
@@ -6,44 +6,70 @@ const mongoose = require('mongoose')
 const Player = require('../models/player')
 const config = require('../utils/config')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+// mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 
-const TEAMS_URL = 'https://statsapi.web.nhl.com/api/v1/teams'
-const EXPAND_ROSTER = '?expand=team.roster'
+const GAMES_URL = 'https://statsapi.web.nhl.com/api/v1/schedule?date=2019-01-10'
+const BOXSCORE_URL = (gamePk) => `https://statsapi.web.nhl.com/api/v1/game/${gamePk}/boxscore`
 
-const PLAYERS_URL = 'https://statsapi.web.nhl.com/api/v1/people' 
-const HYDRATE = '?hydrate=stats(splits=statsSingleSeason)'
-
-const fetchPlayer = async (playerId) => {
-  const { data: { people } } = await axios.get(`${PLAYERS_URL}/${playerId}${HYDRATE}`)
-  const { currentTeam, primaryPosition, currentAge, stats, ...info } = people[0]
-  info.currentTeam = people[0].currentTeam.id
-  info.primaryPosition = people[0].primaryPosition.code
-  const { season, stat } = people[0].stats[0].splits[0]
-  const finalStats = {
-    date: Math.floor(new Date().getTime() / 1000),
-    season,
-    ...stat
+const fetchBoxscore = async (gamePk) => {
+  const { data } = await axios.get(BOXSCORE_URL(gamePk))
+  const { teams: { away: { players } } } = data
+  for (const key in players) {
+    const { person } = players[key]
+    console.log(person.fullName)
   }
-  const player = new Player({ ...info, stats: { ...finalStats } })
-  await player.save()
 }
 
-const fetchRoster = async (teamId) => {
-  const { data: { teams } } = await axios.get(`${TEAMS_URL}/${teamId}/${EXPAND_ROSTER}`)
-  const { roster: { roster } } = teams[0]
-  roster.forEach(player => {
-    fetchPlayer(player.person.fullName)
+const fetchGames = async () => {
+  const { data: { dates } } = await axios.get(GAMES_URL)
+  const { games } = dates[0]
+  games.forEach(game => {
+    const { gamePk } = game
+    fetchBoxscore(gamePk)
   })
 }
 
-const fetchTeams = async () => {
-  const { data: { teams } } = await axios.get(TEAMS_URL)
-  teams.forEach(team => {
-    fetchRoster(team.id)
-  })
-}
+fetchGames()
 
-fetchTeams()
+// mongoose.connection.close()
 
-mongoose.connection.close()
+
+
+// ============================
+// const TEAMS_URL = 'https://statsapi.web.nhl.com/api/v1/teams'
+// const EXPAND_ROSTER = '?expand=team.roster'
+
+// const PLAYERS_URL = 'https://statsapi.web.nhl.com/api/v1/people' 
+// const HYDRATE = '?hydrate=stats(splits=statsSingleSeason)'
+
+// const fetchPlayer = async (playerId) => {
+//   const { data: { people } } = await axios.get(`${PLAYERS_URL}/${playerId}${HYDRATE}`)
+//   const { currentTeam, primaryPosition, currentAge, stats, ...info } = people[0]
+//   info.currentTeam = people[0].currentTeam.id
+//   info.primaryPosition = people[0].primaryPosition.code
+//   const { season, stat } = people[0].stats[0].splits[0]
+//   const finalStats = {
+//     date: Math.floor(new Date().getTime() / 1000),
+//     season,
+//     ...stat
+//   }
+//   const player = new Player({ ...info, stats: { ...finalStats } })
+//   await player.save()
+// }
+
+// const fetchRoster = async (teamId) => {
+//   const { data: { teams } } = await axios.get(`${TEAMS_URL}/${teamId}/${EXPAND_ROSTER}`)
+//   const { roster: { roster } } = teams[0]
+//   roster.forEach(player => {
+//     fetchPlayer(player.person.fullName)
+//   })
+// }
+
+// const fetchTeams = async () => {
+//   const { data: { teams } } = await axios.get(TEAMS_URL)
+//   teams.forEach(team => {
+//     fetchRoster(team.id)
+//   })
+// }
+
+// fetchTeams()
--- docs/api-notes.md
+++ docs/api-notes.md
@@ -2,19 +2,26 @@
 
 ## Useful resources
 [Unofficial API docs](https://gitlab.com/dword4/nhlapi/tree/master/)
+
 [Other API docs](https://github.com/erunion/sport-api-specifications/tree/master/nhl)
+
+[Reddit thread](https://www.reddit.com/r/hockey/comments/7p7o70/documenting_the_nhl_api/)
 
 ## Example Endpoints
 [Detailed player stats](https://statsapi.web.nhl.com/api/v1/people/8476887?hydrate=stats(splits=statsSingleSeason))
+
 Complete player stats
 
 [Games of the day](https://statsapi.web.nhl.com/api/v1/schedule?date=2019-01-09)
+
 Use this without date modifier to get the most recent games
 
 [Boxscore](https://statsapi.web.nhl.com/api/v1/game/2018020668/boxscore)
+
 This will be used for updated the player stats after the games
 
 [Game content](https://statsapi.web.nhl.com/api/v1/game/2018020668/content)
+
 Useful for goal videos. Would be cool to show goal videos of favorited players on the page.
 
 
