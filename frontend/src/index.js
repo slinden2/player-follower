5a6d7774d935b9b497a306be448dd55ed4fbee06
Samu <samu.linden@gmail.com>
2019-06-17 10:29:43 +0200

    CSS resets done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { ApolloProvider } from 'react-apollo-hooks'
 import ApolloClient from 'apollo-boost'
 import App from './App'
+import GlobalStyle from './styles/index'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
@@ -10,6 +11,7 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
+    <GlobalStyle />
     <App />
   </ApolloProvider>,
   document.getElementById('root')
