ae79d8ba7a69cf2cdafbba66a8b15c7a6b1cca7b
Samu <samu.linden@gmail.com>
2019-06-24 14:46:34 +0200

    Set up context in the backend for logged in user

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
 const { ApolloServer } = require('apollo-server')
+const jwt = require('jsonwebtoken')
 const config = require('./utils/config')
 const typeDefs = require('./graphql/schema')
 const resolvers = require('./graphql/resolvers')
+const User = require('./models/user')
+
+require('dotenv').config()
+const JWT_SECRET = process.env.JWT_SECRET
 
 console.log('connecting to', config.MONGODB_URI)
 
@@ -20,6 +25,16 @@ mongoose
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer')) {
+      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
+      const currentUser = await User.findById(decodedToken.id).populate(
+        'favoritePlayers'
+      )
+      return { currentUser }
+    }
+  },
 })
 
 server.listen().then(({ url }) => {
