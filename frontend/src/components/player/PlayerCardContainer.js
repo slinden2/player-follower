6ec09855ab983a6d2e6c1c8cf1d7c4b68c661da5
Samu <samu.linden@gmail.com>
2019-08-28 15:09:31 +0200

    View selector cursor changed to pointer

--- frontend/src/components/player/PlayerCardContainer.js
+++ frontend/src/components/player/PlayerCardContainer.js
@@ -6,13 +6,11 @@ import PlayerCard from './PlayerCard'
 import PlayerCardBack from './PlayerCardBack'
 import styled from 'styled-components'
 import colors from '../../styles/colors'
-import variables from '../../styles/variables'
 
 const Container = styled.div`
   background: ${colors.grey2};
   padding: 1px 10px;
   border-radius: 10px;
-  /* margin-top: ${variables.topMarginWithNav}px; */
 
   .view-selector {
     text-align: center;
@@ -40,6 +38,10 @@ const Container = styled.div`
     grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
     grid-gap: 20px;
   }
+`
+
+const ViewSelectorItem = styled.span`
+  cursor: pointer;
 `
 
 const PlayerCardContainer = ({ query, header }) => {
@@ -76,9 +78,15 @@ const PlayerCardContainer = ({ query, header }) => {
       <Container>
         <h2>{header}</h2>
         <div className="view-selector">
-          <span onClick={() => setCurrentView('Last game')}>Last game</span>
-          <span onClick={() => setCurrentView('Five games')}>5 games</span>
-          <span onClick={() => setCurrentView('Ten games')}>10 games</span>
+          <ViewSelectorItem onClick={() => setCurrentView('Last game')}>
+            Last game
+          </ViewSelectorItem>
+          <ViewSelectorItem onClick={() => setCurrentView('Five games')}>
+            5 games
+          </ViewSelectorItem>
+          <ViewSelectorItem onClick={() => setCurrentView('Ten games')}>
+            10 games
+          </ViewSelectorItem>
         </div>
         <div className="card-container">{createRow(playersToShow)}</div>
       </Container>
