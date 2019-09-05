fceda720c23b0063572acefebfd2070e18254e1b
Samu <samu.linden@gmail.com>
2019-09-05 10:50:53 +0200

    Change teamStats to isTeamStats

--- frontend/src/components/team/TeamProfile.js
+++ frontend/src/components/team/TeamProfile.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from 'react-apollo-hooks'
+import { Link } from 'react-router-dom'
 import { TEAM_PROFILE } from '../../graphql/queries'
 import PageContainer from '../elements/PageContainer'
+import StatsTable from '../StatsTable'
+import { playerStatsHeaders, statHeaders } from '../../utils'
 
 const TeamProfile = ({ siteLink }) => {
+  const [sortVariables, setSortVariables] = useState({
+    offset: 0,
+    sortBy: 'points',
+    sortDir: 'DESC',
+  })
   const { data, loading } = useQuery(TEAM_PROFILE, {
     variables: { siteLink },
   })
@@ -12,9 +20,39 @@ const TeamProfile = ({ siteLink }) => {
     return <div>Loading...</div>
   }
 
-  console.log(data)
+  const sortPlayers = players => {
+    const sortedPlayers = players.sort((a, b) => {
+      let sort
+      sortVariables.sortDir === 'DESC'
+        ? (sort = b[sortVariables.sortBy] - a[sortVariables.sortBy])
+        : (sort = a[sortVariables.sortBy] - b[sortVariables.sortBy])
+      return sort
+    })
+    return sortedPlayers
+  }
 
-  return <PageContainer title="Team"></PageContainer>
+  const index = playerStatsHeaders.indexOf('team')
+  const headers = playerStatsHeaders
+    .slice(0, index)
+    .concat(playerStatsHeaders.slice(index + 1))
+
+  const playersWithLink = data.GetTeam.rosterStats.map(player => ({
+    ...player,
+    fullName: <Link to={`/players/${player.siteLink}`}>{player.fullName}</Link>,
+  }))
+
+  return (
+    <PageContainer title={data.GetTeam.name}>
+      <StatsTable
+        title="Player performance"
+        headers={headers}
+        data={sortPlayers(playersWithLink)}
+        sortVariables={sortVariables}
+        setSortVariables={setSortVariables}
+        sortOnClient={true}
+      />
+    </PageContainer>
+  )
 }
 
 export default TeamProfile
