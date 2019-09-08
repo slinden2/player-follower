15580608c6a7b8ed6a7dc87508e6ee3726667723
Samu <samu.linden@gmail.com>
2019-09-08 20:49:54 +0200

    Link component supports also non links

--- frontend/src/components/elements/StyledLink.js
+++ frontend/src/components/elements/StyledLink.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { Link } from 'react-router-dom'
 
-const SLink = styled(Link)`
+const linkStyling = css`
   background-image: linear-gradient(
     rgba(0, 0, 0, 0) 70%,
     rgba(108, 129, 203, 1) 0px
@@ -13,11 +13,22 @@ const SLink = styled(Link)`
   }
 `
 
-const StyledLink = ({ to, name }) => {
-  return (
+const SLink = styled(Link)`
+  ${linkStyling}
+`
+
+const Span = styled.span`
+  ${linkStyling}
+  cursor: pointer;
+`
+
+const StyledLink = ({ to, name, onClick }) => {
+  return to ? (
     <SLink to={to} name={name}>
       {name}
     </SLink>
+  ) : (
+    <Span onClick={onClick}>{name}</Span>
   )
 }
 
