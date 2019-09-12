2b5a560f36f16ad0f47d82fa181edc7180130c19
Samu <samu.linden@gmail.com>
2019-09-12 18:05:15 +0200

    Added html email template

--- backend/utils/mailgun-email-sender.js
+++ backend/utils/mailgun-email-sender.js
@@ -1,4 +1,6 @@
 const mailgun = require('mailgun-js')
+const generateAccountConfirmationEmail = require('./templates/account-activation-email')
+const generateForgotPasswordEmail = require('./templates/forgot-password-email')
 
 let domain = 'http://www.player.fan'
 if (process.env.NODE_ENV !== 'production') {
@@ -9,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 const MG_CONN = mailgun({
   apiKey: process.env.MG_API_KEY,
   domain: process.env.MG_DOMAIN,
-  // host: 'api.eu.mailgun.net',
+  host: 'api.eu.mailgun.net',
 })
 
 const sendVerificationEmail = (email, token) => {
@@ -18,6 +20,7 @@ const sendVerificationEmail = (email, token) => {
     to: 'playerfansite@gmail.com',
     subject: 'Account confirmation',
     text: `Please visit the following to to activate your account: ${domain}/confirmation/${token}`,
+    html: generateAccountConfirmationEmail(domain, token),
   }
 
   MG_CONN.messages().send(data, (error, body) => {
@@ -31,6 +34,7 @@ const sendForgotPasswordEmail = async (email, token) => {
     to: 'playerfansite@gmail.com',
     subject: 'Change your password with this mail',
     text: `Please visit the following link to change your password: ${domain}/forgot-password/${token}`,
+    html: generateForgotPasswordEmail(domain, token),
   }
 
   MG_CONN.messages().send(data, function(error, body) {
@@ -38,4 +42,27 @@ const sendForgotPasswordEmail = async (email, token) => {
   })
 }
 
-module.exports = { sendVerificationEmail, sendForgotPasswordEmail }
+const sendContactFormEmail = async (
+  name,
+  email,
+  subject,
+  message,
+  username
+) => {
+  const data = {
+    from: 'Player Fan Contact Form <noreply@player.fan>',
+    to: 'contact@player.fan',
+    subject,
+    text: `Username: ${username}\nName: ${name}\nEmail: ${email}\n\nMessage: ${message}`,
+  }
+
+  MG_CONN.messages().send(data, function(error, body) {
+    console.log(body)
+  })
+}
+
+module.exports = {
+  sendVerificationEmail,
+  sendForgotPasswordEmail,
+  sendContactFormEmail,
+}
