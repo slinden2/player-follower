369efe1867e61dff81df19bb066f0a453e82d59e
Samu <samu.linden@gmail.com>
2019-09-15 16:33:02 +0200

    Login fix

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect, useContext } from 'react'
+import React, { createContext, useState, useContext } from 'react'
 import { useApolloClient, useQuery } from 'react-apollo-hooks'
 import { setCookie, getCookie, removeCookie } from '../utils'
 import { USER } from '../graphql/queries'
@@ -7,17 +7,11 @@ import { PlayerContext } from './PlayerContext'
 export const AuthContext = createContext()
 
 const AuthContextProvider = props => {
-  const [token, setToken] = useState(null)
+  const [token, setToken] = useState(getCookie('user'))
   const { favoritePlayers } = useContext(PlayerContext)
 
   const client = useApolloClient()
   const user = useQuery(USER)
-
-  // login with cookie
-  useEffect(() => {
-    const tokenCookie = getCookie('user')
-    setToken(tokenCookie)
-  }, [])
 
   // login user and query favorite players
   const loginUser = async (token, rememberMe) => {
