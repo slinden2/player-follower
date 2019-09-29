e9fd3d3c1db2f03e1c68214bb78ae5f3872bcdea
Samu <samu.linden@gmail.com>
2019-09-29 11:16:26 +0200

    GA tracking number setup

--- frontend/src/utils/tracking.js
+++ frontend/src/utils/tracking.js
@@ -2,17 +2,20 @@ import ReactGA from 'react-ga'
 import { getCookie, setCookie } from '../utils/index'
 
 export const initGA = () => {
-  if (process.env.NODE_ENV === 'production' && process.env.GA !== 'disabled') {
-    if (getCookie('gaConsent')) {
+  console.log(process.env)
+  if (getCookie('gaConsent')) {
+    if (process.env.NODE_ENV === 'production') {
       ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_NO)
-
-      // fl (first load) cookie allows to record the first pageview after accepting cookies
-      if (!getCookie('fl')) {
-        ReactGA.pageview(window.location.pathname)
-      }
-
-      setCookie('fl', true, 365)
+    } else {
+      ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_NO_DEV)
     }
+
+    // fl (first load) cookie allows to record the first pageview after accepting cookies
+    if (!getCookie('fl')) {
+      ReactGA.pageview(window.location.pathname)
+    }
+
+    setCookie('fl', true, 365)
   }
 }
 
--- run-fetch-boxscores.py
+++ run-fetch-boxscores.py
@@ -7,9 +7,9 @@ FILE_PATH = os.path.join(os.path.dirname(
 os.chdir(FILE_PATH)
 
 # day before first fetch date
-date = datetime.datetime(2019, 3, 9)
+date = datetime.datetime(2019, 3, 10)
 
-for i in range(0, 16):
+for i in range(0, 20):
     date += datetime.timedelta(days=1)
     dateStr = date.strftime('%Y-%m-%d')
     os.system(f'npm run fetch_boxscores {date}')
