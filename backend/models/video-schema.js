80b8efc88226de378d26c4f7a928129047461cfd
Samu <samu.linden@gmail.com>
2019-10-01 21:05:42 +0200

    Added games and milestones

---
+++ backend/models/video-schema.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const playbackSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    width: {
+      type: {},
+    },
+    height: {
+      type: {},
+    },
+    url: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { id_: false }
+)
+
+playbackSchema.plugin(uniqueValidator)
+
+const videoSchema = mongoose.Schema({
+  videoId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  blurb: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  mediaPlaybackId: {
+    type: Number,
+    required: true,
+  },
+  playbacks: [playbackSchema],
+})
+
+videoSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+videoSchema.plugin(uniqueValidator)
+
+module.exports = videoSchema
