2210283efa0ff64097f3550b0b49c154c0563393
Samu <samu.linden@gmail.com>
2019-10-10 11:50:26 +0200

    Created containers for dropdowns

--- frontend/src/components/player/card/PlayerCardPage.js
+++ frontend/src/components/player/card/PlayerCardPage.js
@@ -14,13 +14,32 @@ import {
 import PlayerCardContainer from './PlayerCardContainer'
 import Loader from '../../elements/Loader'
 import { LAST_UPDATE } from '../../../graphql/queries'
+import breakpoints from '../../../styles/breakpoints'
+import colors from '../../../styles/colors'
 
 const Container = styled.div`
   position: relative;
 `
 
 const FilterContainer = styled.div`
-  text-align: center;
+  @media ${breakpoints.showDesktopNavi} {
+    display: flex;
+    justify-content: center;
+  }
+`
+
+const FieldContainer = styled.fieldset`
+  font-size: 0.875rem;
+  border: 2px solid ${colors.grey1};
+  border-radius: 10px;
+  margin: 5px;
+`
+
+const FieldsetTitle = styled.legend`
+  border: 2px solid ${colors.grey1};
+  border-radius: 10px;
+  background-color: ${colors.grey1};
+  padding: 5px;
 `
 
 const LastUpdated = styled.div`
@@ -80,26 +99,32 @@ const PlayerCardPage = ({ queryName, header }) => {
           setCurrentView={setNumOfGames}
         />
         <FilterContainer>
-          <DropdownMenu
-            items={sortByItems}
-            state={sortBy}
-            setState={setSortBy}
-          />
-          <DropdownMenu
-            items={playerPosFilterItems}
-            state={positionFilter}
-            setState={setPositionFilter}
-          />
-          <DropdownMenu
-            items={playerTeamFilterItems}
-            state={teamFilter}
-            setState={setTeamFilter}
-          />
-          <DropdownMenu
-            items={playerNationalityFilterItems}
-            state={nationalityFilter}
-            setState={setNationalityFilter}
-          />
+          <FieldContainer>
+            <FieldsetTitle>Sort</FieldsetTitle>
+            <DropdownMenu
+              items={sortByItems}
+              state={sortBy}
+              setState={setSortBy}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <FieldsetTitle>Filters</FieldsetTitle>
+            <DropdownMenu
+              items={playerPosFilterItems}
+              state={positionFilter}
+              setState={setPositionFilter}
+            />
+            <DropdownMenu
+              items={playerTeamFilterItems}
+              state={teamFilter}
+              setState={setTeamFilter}
+            />
+            <DropdownMenu
+              items={playerNationalityFilterItems}
+              state={nationalityFilter}
+              setState={setNationalityFilter}
+            />
+          </FieldContainer>
         </FilterContainer>
         <PlayerCardContainer queryName={queryName} />
       </Container>
