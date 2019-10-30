f9ae669fa7a4d001f2169c6eacd9bac71a0d226f
Samu <samu.linden@gmail.com>
2019-10-30 19:35:00 +0100

    Tweet testing

---
+++ web/build/precache-manifest.abb07afc11bb398eb541f5448c44aa5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653d86ab1e9540dc79742e2fb3948253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f47977e99f3e2eda54b",
+    "url": "/static/js/2.6da29f48.chunk.js"
+  },
+  {
+    "revision": "8b7cbe8f26f60babb80b",
+    "url": "/static/js/main.417b05d4.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  },
+  {
+    "revision": "b6a65f37f1176b19d9c12d3a68879a9e",
+    "url": "/static/media/magnifying-glass-icon.b6a65f37.svg"
+  }
+]);
